09c3fe613c3539c2a3688eb8474fd2a4312f359b
snerpent <snerpent17@gmail.com>
2015-12-05 23:31:38 -0800

    Added Challenge 7

---
+++ easy/easy7.js
@@ -0,0 +1,28 @@
+function test() {
+	console.log("This will be fun once I learn it.")
+}
+
+var morseKey = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+
+var alphaKey = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+function code(){
+	var stepString;
+
+	console.log("code works");
+}
+
+function decode(){
+	var stepString;
+	if " / ", then
+		take the string
+
+	console.log("decode works");
+
+}
+
+test();
+code();
+decode();
+
