5fdc010c401b305c33fc40c34be36307c57680bb
snerpent <snerpent17@gmail.com>
2015-12-06 20:58:59 -0800

    Challenge 7 Update

--- easy/easy7.js
+++ easy/easy7.js
@@ -8,37 +8,78 @@ var alphaKey = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 
 
 function code(){
-	var stepString;
+	var alphaString = "HELLO THIS IS A TEST";
+	var morseString = "";
+
+	while (alphaString = "") {
+
+		for (i = 0; i < alphaString.length; i++){
+
+			for (j = 0; j < alphaKey.length; j++) {
+
+
+				if (alphaString[i] == " "){
+				
+					if (alphaString[i] == alphaKey[j]) {
+
+							morseString += morseKey[j];
+
+					}
+
+
+
+				}
+			}
+		}
+
+
+
+	}
 
 	console.log("code works");
 }
 
 function decode(){
-	var codedString = ".... .-.. .-.. ---";
-	var letter = codedString.split(" ");
+	var codedString = ".... . .-.. .-.. ---";
 	var solvedString = "";
+	
+	while (codedString != "") {
+		while (codedString.search (" ") != -1) {
+			if (codedString.substring(0, 3) == " / " ) {
+				solvedString += " ";
+				codedString = codedString.substring(0,3).pop();
+			}
 
+			else{
 
-	if (codedString.substring(0, 3) == " / " ) {
-		solvedString += " ";
-	}
-
-	else{
-
-		for (i = 0; i < morseKey.length; i++){
-			if (codedString.split(" ", 1) == morseKey[i]){
-				solvedString += alphaKey[i];
-				codedString = codedString.slice(0, codedString.split(" ", 1).length);
+				for (i = 0; i < morseKey.length; i++){
+					if (codedString.split(" ", 1) == morseKey[i]){
+						solvedString += alphaKey[i];
+					}
+				}
+				
+				codedString = codedString.slice(codedString.split(" ", 1).pop().length+1, codedString.length);
+				
+			}
 		}
-		
-
+		for (i = 0; i < morseKey.length; i++){
+			if (codedString == morseKey[i]){
+				solvedString += alphaKey[i];
+				codedString = "";
+			}
+		}
 	}
+	
+	//while (codedString.search(" ") != -1);
 
 	console.log("The answer is " + solvedString);
+	console.log(codedString);
+	
+	
 
 }
 
 test();
-code();
 decode();
+code();
 
